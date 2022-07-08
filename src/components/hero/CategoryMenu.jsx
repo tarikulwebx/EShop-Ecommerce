8d80640f89bda72f7827bd2d90f5105d8232d855
Tarikul <tarikulwebx@gmail.com>
2022-07-08 18:53:51 +0600

    Hero with category added

---
+++ src/components/hero/CategoryMenu.jsx
@@ -0,0 +1,118 @@
+import { alpha, List, ListItem, ListItemButton, ListItemIcon, Paper, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import React from 'react'
+import { FaBaby, FaGamepad, FaGifts, FaPlug, FaTshirt } from "react-icons/fa";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
+
+
+const categories = [
+	{
+		name: "Fashion",
+		icon: <FaTshirt />,
+	},
+	{
+		name: "Electronics",
+		icon: <FaPlug />,
+	},
+	{
+		name: "Gadgets",
+		icon: <FaGamepad />,
+	},
+
+	{
+		name: "Gifts",
+		icon: <FaGifts />,
+	},
+	{
+		name: "Baby Toys",
+		icon: <FaBaby />,
+	},
+    {
+		name: "Gadgets",
+		icon: <FaGamepad />,
+	},
+
+	{
+		name: "Gifts",
+		icon: <FaGifts />,
+	},
+	{
+		name: "Baby Toys",
+		icon: <FaBaby />,
+	},
+    
+];
+
+const CategoryMenu = () => {
+    return (
+		<Paper
+			elevation={2}
+			sx={{
+				borderRadius: 3,
+				minWidth: "220px",
+				".simplebar-track.simplebar-vertical": {
+					width: "8px",
+				},
+				".simplebar-scrollbar:before": {
+					backgroundImage: (theme) =>
+						"linear-gradient(" +
+						theme.palette.primary.light +
+						", " +
+						theme.palette.secondary.light +
+						")",
+				},
+				display: { xs: "none", md: "block" },
+			}}
+		>
+			<SimpleBar style={{ height: "400px" }}>
+				<List
+					sx={{
+						minWidth: "220px",
+						height: "100%",
+					}}
+				>
+					{categories.map((category, i) => (
+						<ListItem
+							key={i}
+							sx={{
+								p: 0,
+							}}
+						>
+							<ListItemButton
+								sx={{
+									color: grey[700],
+									textTransform: "capitalize",
+									"&:hover, &.active": {
+										color: "primary.main",
+										backgroundColor: (theme) =>
+											alpha(
+												theme.palette.primary.main,
+												0.04
+											),
+									},
+								}}
+							>
+								<ListItemIcon
+									sx={{
+										minWidth: 0,
+										mr: 2.5,
+										fontSize: "1.125rem",
+										color: grey[600],
+									}}
+								>
+									{category.icon}
+								</ListItemIcon>
+								<Typography sx={{ fontSize: "0.975rem" }}>
+									{category.name}
+								</Typography>
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
+			</SimpleBar>
+		</Paper>
+	);
+}
+
+export default CategoryMenu
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,17 @@
-import { Box, Container } from '@mui/material'
+import { Box, Container
+ } from '@mui/material'
 import React from 'react'
+import CategoryMenu from './CategoryMenu';
+import HeroSlider from './HeroSlider';
 
 const Hero = () => {
     return (
-		<Box component="section" sx={{}}>
-			<Container maxWidth="xl" sx={{ minHeight: "800px" }}>
-				Hero
+		<Box component="section" sx={{ py: 3 }}>
+			<Container maxWidth="xl">
+				<Box sx={{ display: "flex", gap: 3 }}>
+					<CategoryMenu />
+					<HeroSlider />
+				</Box>
 			</Container>
 		</Box>
 	);
